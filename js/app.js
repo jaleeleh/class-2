47c5b0d3442c0d8bcdf00ab0fccba1bc63011fa9
jaleeleh <jaleeleha@gmail.com>
2021-02-09 15:48:55 +0200

    add some changes

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 'use strict';
-
+let score=0;
 let userName=prompt('what is your name ?');
 //console.log('userName')
 alert('hello '+userName+' now we will play quessing game  please answer by yes or no')
 
-let Q1=prompt('am i a male?');
+let Q1=prompt('is my name maria?');
 console.log('Q1');
 switch(Q1.toLowerCase()){
     case 'yes':
     case 'y':
         alert(' wrong answer');
+          
         break;
     case 'no':
         
         alert('correct answer');
+        score++;
         break;
 
 }
@@ -22,11 +24,15 @@ console.log('Q1');
 switch(Q1.toLowerCase()){
     case 'yes':
     case 'y':
+
         alert('wrong answer');
         break;
     case 'no':
     case 'n':
         alert('correct answer');
+        score++;
+        
+        
         break;
 
 }
@@ -37,6 +43,7 @@ switch(Q3.toLowerCase()){
     case 'yes':
     case 'y':
         alert(' correct answer');
+score++;
         break;
     case 'no':
     case 'n':
@@ -50,6 +57,7 @@ switch(Q4.toLowerCase()){
     case 'yes':
     case 'y':
         alert(' correct answer');
+score++;
         break;
     case 'no':
     case 'n':
@@ -63,6 +71,7 @@ switch(Q5.toLowerCase()){
     case 'yes':
     case 'y':
         alert(' correct answer');
+score++;
         break;
     case 'no':
     case 'n':
@@ -70,6 +79,65 @@ switch(Q5.toLowerCase()){
         break;
 
 }
+
+let Q6=prompt('how old are me?');
+Q6=Number(Q6);
+ 
+
+    for (let i=0;i<4;i++)
+     {
+        if (Q6==28)
+        { 
+            alert('correct answer');
+            score++;
+            break;
+        }else if (Q6>28)
+            { 
+             alert('too old');
+             Q6=prompt('please try again?');
+
+            } else
+            { 
+              alert('too young');
+              Q6=prompt('please try again?');
+            }
+     }
+    alert('the correct answer is '+28);
+
+
+
+let counteries=['iraq','palestine','turkey','jeorgia'];
+let Q7=prompt('write down one country that i have visited befor?');
+let check=true;
+    
+        
+        for (let ii=0;ii<6;ii++){
+            for (let i= 0; i < counteries.length; i++) {
+               if(Q7==counteries[i]) {
+                alert('correct answer');
+                check=false;
+                score++;
+                break;
+               }
+            }if(check){
+
+            Q7=prompt('please try again?');
+        }
+        else{break;}
+    }
+        
+                
+            alert('the countries that i have visied :  '+'iraq and palestine and turkey and georgia');
+            alert('your score is: '+score);
+
+
+
+    
+
+
+
+    
+
 
 
 
