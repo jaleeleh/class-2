5c62db9e0953b59d19861271ddfe744a23d311a5
RaneemAbdulazez <raneem97su@gmail.com>
2021-02-10 16:28:56 +0200

    functions added

--- js/app.js
+++ js/app.js
@@ -3,7 +3,17 @@ let score=0;
 let userName=prompt('what is your name ?');
 //console.log('userName')
 alert('hello '+userName+' now we will play quessing game  please answer by yes or no')
+Q1();
+Q2();
+Q3();
+Q4();
+Q5();
+Q6();
+Q7();
 
+
+
+function Q1(){
 let Q1=prompt('is my name maria?');
 console.log('Q1');
 switch(Q1.toLowerCase()){
@@ -19,9 +29,15 @@ switch(Q1.toLowerCase()){
         break;
 
 }
+return;
+}
+
+
+function Q2(){
+
 let Q2=prompt('does polar bear lives with penguin in the same area ?');
 console.log('Q1');
-switch(Q1.toLowerCase()){
+switch(Q2.toLowerCase()){
     case 'yes':
     case 'y':
 
@@ -36,7 +52,10 @@ switch(Q1.toLowerCase()){
         break;
 
 }
+return;
+}
 
+function Q3(){
 let Q3=prompt('does snail sleep for 3 years?');
 console.log('Q3');
 switch(Q3.toLowerCase()){
@@ -51,6 +70,10 @@ score++;
         break;
 
 }
+return;
+}
+
+function Q4(){
 let Q4=prompt('does cates can rotate their ears 180 degrees?');
 console.log('Q4');
 switch(Q4.toLowerCase()){
@@ -65,6 +88,10 @@ score++;
         break;
 
 }
+return;
+}
+
+function Q5(){
 let Q5=prompt('does the moon surface actually dark?');
 console.log('Q5');
 switch(Q5.toLowerCase()){
@@ -79,13 +106,16 @@ score++;
         break;
 
 }
+return;
+}
 
-let Q6=prompt('how old are me?');
-Q6=Number(Q6);
+function Q6(){
+
  
 
     for (let i=0;i<4;i++)
      {
+        let Q6=prompt('how old are me?');
         if (Q6==28)
         { 
             alert('correct answer');
@@ -103,15 +133,20 @@ Q6=Number(Q6);
             }
      }
     alert('the correct answer is '+28);
+return;
+    }
 
 
+
+function Q7(){
 
 let counteries=['iraq','palestine','turkey','jeorgia'];
-let Q7=prompt('write down one country that i have visited befor?');
+
 let check=true;
     
         
         for (let ii=0;ii<6;ii++){
+            let Q7=prompt('write down one country that i have visited befor?');
             for (let i= 0; i < counteries.length; i++) {
                if(Q7==counteries[i]) {
                 alert('correct answer');
@@ -129,8 +164,8 @@ let check=true;
                 
             alert('the countries that i have visied :  '+'iraq and palestine and turkey and georgia');
             alert('your score is: '+score);
-
-
+return;
+}
 
     
 
